b19a53ff50b8e78d837cddae1149a1cfc3506b62
andrepitombeira <andrepitombeira@gmail.com>
2012-07-25 18:30:01 -0300

    Add internationalization

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ActiveRecord::Base
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {with: %r{\.(gif|jpg|png)$}i, message: 'must be a URL for GIF, JPG or PNG image.'}
   has_many :line_items
+  has_many :orders, through: :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
   
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,36 @@
 Depot::Application.routes.draw do
+  get 'admin' => 'admin#index'
+    
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+  
+  scope '(:locale)' do
+    resources :users
+    resources :orders
+    resources :line_items
+    resources :carts
+    resources :products do
+      get :who_bought, on: :member
+    end
+    root to: 'store#index', as: 'store'
+  end
+
+  resources :users
+
+  resources :orders
+
   resources :line_items
 
   resources :carts
 
   get "store/index"
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end 
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
